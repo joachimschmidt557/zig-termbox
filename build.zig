7df6323ceb13d1cbe1709f433cd647c525e89099
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-05-11 09:37:06 +0800

    Add examples to build.zig

--- build.zig
+++ build.zig
@@ -1,5 +1,11 @@
 const Builder = @import("std").build.Builder;
 
+const examples = [_][]const u8{
+    "hello",
+    "paint",
+    "input",
+};
+
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("zig-termbox", "src/main.zig");
@@ -11,4 +17,13 @@ pub fn build(b: *Builder) void {
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+    const examples_step = b.step("examples", "Install examples");
+    inline for (examples) |example| {
+        var exe = b.addExecutable(example, "examples/" ++ example ++ ".zig");
+        exe.addPackagePath("termbox", "src/main.zig");
+        exe.setBuildMode(mode);
+        exe.install();
+    }
+    examples_step.dependOn(b.getInstallStep());
 }
